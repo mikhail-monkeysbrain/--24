936f8475146ee6a2e0c90bfba81a425dd2b8a5a7
monkeysbrain <monkeysbrain@yandex.ru>
2016-12-29 12:34:09 +0500

    2-3 page

--- app/js/app.js
+++ app/js/app.js
@@ -4,3 +4,35 @@ $(function(){
         $(this).find('p').toggleClass("bw")
     });
 })
+$(function(){
+  $('.popup-map-icons--item').height($('.popup-map-icons--item').width()/1);
+  $(window).resize(function(){
+    $('.popup-map-icons--item').height($('.popup-map-icons--item').width()/1);
+  });
+});
+$(function(){
+	$('#popup').hide();
+    $('.burger').click(function() {
+    	$(this).hide(500);
+        $('#left-menu').toggleClass("open");
+    });
+    $('.close').click(function() {
+    	$('.burger').show(500);
+        $('#left-menu').toggleClass("open");
+	});
+    $('.map').click(function() {
+    	$('.burger').show(500);
+        $('#left-menu').toggleClass("open");
+    	$('#popup').show(500);
+	});
+    $('.popup-map-icons--item-trans').click(function() {
+    	$('#popup').hide(300);
+	});
+
+})
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+	    loopTop: true,
+	    loopBottom: true
+	});
+});
